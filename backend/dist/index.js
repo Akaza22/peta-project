13dbdc7719e4c86af50f92fa22311c7a83e5376f
Akaza22 <rmrbintang22@gmail.com>
2025-01-03 15:07:31 +0700

    vercel.json

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import authRoutes from './routes/authRoutes';
+import authRoutes from './routes/authRoutes.js';
 import sequelize from './database/db';
 dotenv.config();
 const app = express();
