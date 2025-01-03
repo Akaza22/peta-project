2e2f9b55d95b99fabdb6211d8e205f9408fa4af5
Akaza22 <rmrbintang22@gmail.com>
2025-01-03 16:18:46 +0700

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,7 +20,7 @@ app.use('/auth', authRoutes);
 
 
 sequelize.sync().then(() => {
-  console.log('Databas connected & tables synced');
+  console.log('Database connected & tables synced');
 });
 
 app.listen(PORT, () => {
