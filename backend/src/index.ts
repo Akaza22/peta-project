a0c3f771fe6b8b7333fb680ae27208176b97db5b
Akaza22 <rmrbintang22@gmail.com>
2025-01-03 16:27:16 +0700

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,10 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: '*', // Mengizinkan semua domain
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}))
+app.use(cors());
 app.use(express.json());
 app.use('/auth', authRoutes);
 
