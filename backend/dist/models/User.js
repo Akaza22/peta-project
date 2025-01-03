85af69160ecbacaa5c56048b450ff6d8eac8ddae
Akaza22 <rmrbintang22@gmail.com>
2025-01-03 15:02:48 +0700

    dist

---
+++ backend/dist/models/User.js
@@ -0,0 +1,30 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../database/db';
+class User extends Model {
+}
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+}, {
+    sequelize,
+    tableName: 'users',
+    timestamps: true, // Aktifkan timestamps agar Sequelize mengelola createdAt dan updatedAt
+});
+export default User;
